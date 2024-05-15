917725725d9731601566457d586fcd7f6e494867
Vishrut Jha <vishrutkmr7@icloud.com>
2024-05-15 18:30:08 -0500

    Initial Commit

---
+++ MultiPlaycation/MultiPlaycationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiPlaycationApp.swift
+//  MultiPlaycation
+//
+//  Created by Vishrut Jha on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MultiPlaycationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
